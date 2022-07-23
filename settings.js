76cf1482b2ef8b44bdf8ad4dbf93a1468dfee207
apnavpn <107626617+apnavpn@users.noreply.github.com>
2022-07-23 15:29:03 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
+global.owner = ['923154376814']
 global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.ownernomer = 'Yasir Tanveer'
+global.ownername = '🐦Yasir Tanveer'
+global.botname = 'Apna Bot'
+global.footer = '©YasirTanveer.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Pakistan, Lahore'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/c/yasircomputertips'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
